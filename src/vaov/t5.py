a45c8a25f1e431491dc3bda564dca46390c1a45f
nev <sshabalin3@gatech.edu>
2024-09-15 00:01:46 +0000

    Measure deviation correctly, remove stuff

--- src/vaov/t5.py
+++ src/vaov/t5.py
@@ -27,28 +27,13 @@ def quantize_params_tree(params):
     return jax.tree_util.tree_map_with_path(maybe_quantize, params)
 
 if __name__ == "__main__":
-    # with jax.default_device(jax.devices("cpu")[0]):
-    #     # model_name = "google/t5-v1_1-xxl"
-    #     # model = FlaxT5EncoderModel.from_pretrained(model_name, from_pt=True)
-    #     # model.params = jax.tree.map(lambda x: x.astype(jnp.bfloat16), model.params)
-    #     # model.save_pretrained("t5-v1_1-xxl-flax", params=model.params, push_to_hub=True)
-    #     tokenizer = AutoTokenizer.from_pretrained("google/t5-v1_1-xxl")
-    #     tokenizer.save_pretrained("t5-v1_1-xxl-flax", push_to_hub=True)
-    # exit()
-
     model_name = "nev/t5-v1_1-xxl-flax"
     model, params = FlaxT5EncoderModel.from_pretrained(model_name, _do_init=False, dtype=jnp.bfloat16)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     quantized_params = quantize_params_tree(params)
-    
-    # TODO add sharding to kernel
-    # basic shmap, could add the new RDMA-based interface
-
-    # mesh = jax.sharding.Mesh(np.asarray(jax.devices()).reshape(-1), ("dp",))
-    # sharding_tree = jax.tree.map(lambda x: jax.sharding.NamedSharding(mesh, jax.sharding.PartitionSpec(*([None]*len(x.shape)))), quantized_params)
-    # quantized_params = jax.device_put(quantized_params, sharding_tree)
 
     quantized_params = jax.device_put(quantized_params, jax.devices("tpu")[0])
+    params = jax.device_put(params, jax.devices("tpu")[0])
     
     wrapped_model = jax.jit(qax.use_implicit_args(model.__call__))
 
@@ -68,35 +53,12 @@ if __name__ == "__main__":
     encoder_input = encoder_input
     decoder_start = decoder_start
 
-    # kwargs = {"input_ids": encoder_input, "attention_mask": encoder_mask, "decoder_input_ids": decoder_start, "decoder_attention_mask": decoder_mask}
-    # kwargs = {"input_ids": encoder_input, "decoder_input_ids": decoder_start}
     kwargs = {"input_ids": encoder_input}
     set_use_kernel(quantized_params, True)
-    def visualize(params, quantized_params, cur_path=()):
-        for k, v in params.items():
-            if isinstance(v, dict):
-                visualize(v, quantized_params[k], cur_path + (k,))
-            else:
-                print(cur_path + (k,), v.shape, quantized_params[k].shape)  #, jnp.mean(jnp.abs(v - quantized_params[k].dequantize())))
-    visualize(params, quantized_params)
     quantized_logits = wrapped_model(params=quantized_params, **kwargs).last_hidden_state
-    print(quantized_logits.keys())
     set_use_kernel(quantized_params, False)
     quantized_logits_no_kernel = wrapped_model(params=quantized_params, **kwargs).last_hidden_state
-    # base_logits                = wrapped_model(params=params, **kwargs).logits
+    base_logits                = wrapped_model(params=params, **kwargs).last_hidden_state
     print(jnp.mean(jnp.abs(quantized_logits - quantized_logits_no_kernel)), jnp.mean(jnp.abs(quantized_logits_no_kernel)))
+    print(jnp.mean(jnp.abs(base_logits - quantized_logits)), jnp.mean(jnp.abs(base_logits)))
     exit()
-
-    print(f"Input: {input}")
-    # for (name, logits) in ("Original", base_logits), ("Quantized", quantized_logits), ("Quantized No Kernel", quantized_logits_no_kernel):
-    for name, logits in (
-        ("Quantized", quantized_logits),
-        ("Quantized No Kernel", quantized_logits_no_kernel),
-    ):
-        last_logits = logits[0][-1]
-        assert last_logits.ndim == 1
-        probs = jax.nn.softmax(last_logits)
-        top_probs, top_tokens = jax.lax.top_k(probs, 10)
-        print(f"{name} predictions")
-        for token, prob in zip(top_tokens, top_probs):
-            print(f"\t{tokenizer.convert_ids_to_tokens(int(token))}: {prob:.2%}")
