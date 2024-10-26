a3a482b9785f8e39c0cdf663dcdea90353a56dcd
nev <sshabalin3@gatech.edu>
2024-10-26 15:26:33 +0000

    Draw the rest of the owl

--- src/vaov/diformer.py
+++ src/vaov/diformer.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass
 from jaxtyping import Array, Float, UInt
 from typing import Optional, Tuple
 import jax.numpy as jnp
+from tqdm.auto import tqdm
 from einops import rearrange
 import jax
 import math
@@ -131,7 +132,7 @@ class EmbedND(eqx.Module):
         return emb[..., None, :, :, :]
 
 
-class VLinear(nn.Linear):
+class VLinear(eqx.Module):
     """Linear layer with optional bias."""
     in_channels: int
     out_channels: int
@@ -148,7 +149,6 @@ class VLinear(nn.Linear):
             use_bias: Whether to use bias.
             key: Random key for initialization.
         """
-        super().__init__(in_channels, out_channels, use_bias=False, key=key)
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.weight = jax.random.normal(key, (in_channels, out_channels)) * (1 / math.sqrt(in_channels))
@@ -187,7 +187,6 @@ class MLPEmbedder(eqx.Module):
             hidden_dim: Hidden dimension.
             key: Random key for initialization.
         """
-        super().__init__()
         self.in_layer = VLinear(in_dim, hidden_dim, use_bias=True, key=key)
         self.out_layer = VLinear(hidden_dim, hidden_dim, use_bias=True, key=key)
 
@@ -219,7 +218,6 @@ class LastLayer(eqx.Module):
     adaLN_modulation: eqx.Module
 
     def __init__(self, hidden_size: int, patch_size: int, out_channels: int, *, key: jax.random.PRNGKey):
-        super().__init__()
         self.norm_final = VLayerNorm(hidden_size, use_weight=False, use_bias=False, eps=1e-6)
         self.linear = VLinear(hidden_size, patch_size * patch_size * out_channels, use_bias=True, key=key)
         self.adaLN_modulation = nn.Sequential((
@@ -237,8 +235,8 @@ class LastLayer(eqx.Module):
 class QKNorm(eqx.Module):
     query_norm: nn.RMSNorm
     key_norm: nn.RMSNorm
+
     def __init__(self, dim: int):
-        super().__init__()
         self.query_norm = nn.RMSNorm(dim)
         self.key_norm = nn.RMSNorm(dim)
 
@@ -287,18 +285,113 @@ class Modulation(eqx.Module):
         )
 
 
+class SelfAttention(eqx.Module):
+    """Self-attention"""
+    qk_norm: QKNorm
+    qkv_proj: VLinear
+    o_proj: VLinear
+    head_dim: int
+    
+    def __init__(self, dim: int, num_heads: int, qkv_bias: bool = False, *, key: jax.random.PRNGKey):
+        self.head_dim = dim // num_heads
+        self.qkv_proj = VLinear(dim, dim * 3, use_bias=qkv_bias, key=key)
+        self.o_proj = VLinear(dim, dim, key=key)
+        self.qk_norm = QKNorm(self.head_dim)
+
+    def qkv(self, x):
+        qkv = self.qkv_proj(x)
+        q, k, v = rearrange(
+            qkv,
+            "... (qkv heads dim) -> qkv ... heads dim",
+            qkv=3,
+            dim=self.head_dim,
+        )
+        q, k = self.qk_norm(q, k, v)
+        return q, k, v
+
+    def __call__(self, x, pe):
+        q, k, v = self.qkv(x)
+        attn = attention(q, k, v, pe=pe)
+        return self.o_proj(attn)
+
+class MLP(eqx.Module):
+    in_proj: VLinear
+    out_proj: VLinear
+    
+    def __init__(self, config: DiFormerConfig, *, key: jax.random.PRNGKey):
+        mlp_hidden_dim = int(config.hidden_size * config.mlp_ratio)
+        self.in_proj = VLinear(
+            config.hidden_size,
+            mlp_hidden_dim,
+            key=key,
+            use_bias=True,
+        )
+        self.out_proj = VLinear(
+            mlp_hidden_dim,
+            config.hidden_size,
+            key=key,
+            use_bias=True
+        )
+    
+    def __call__(self, x):
+        mlp = self.in_proj(x)
+        mlp = jax.nn.gelu(mlp, approximate=True)
+        return self.out_proj(mlp)
+
+
 class DoubleStreamBlock(eqx.Module):
     """Main two-stream MMDiT block."""
-    weight_img: VLinear
-    weight_txt: VLinear
+
+    img_mod: Modulation
+    img_norm1: VLayerNorm
+    img_attn: SelfAttention
+    img_norm2: VLayerNorm
+    img_mlp: MLP
+    txt_mod: Modulation
+    txt_norm1: VLayerNorm
+    txt_attn: SelfAttention
+    txt_norm2: VLayerNorm
+    txt_mlp: MLP
+
     def __init__(self, config: DiFormerConfig, *, key: jax.random.PRNGKey):
-        self.weight_img = VLinear(config.hidden_size, config.hidden_size, key=key)
-        self.weight_txt = VLinear(config.hidden_size, config.hidden_size, key=key)
-    
+        self.img_norm1, self.img_norm2, self.txt_norm1, self.txt_norm2 = \
+            (VLayerNorm(config.hidden_size, use_weight=False, use_bias=False, eps=1e-6) for _ in range(4))
+        self.img_mod, self.txt_mod = (
+            Modulation(config.hidden_size, double=True, key=k) for k in jax.random.split(key, 2))
+        self.img_attn, self.txt_attn = (
+            SelfAttention(config.hidden_size, config.num_heads, qkv_bias=config.qkv_bias, key=k) for k in jax.random.split(key, 2))
+        self.img_mlp, self.txt_mlp = (
+            MLP(config, key=k) for k in jax.random.split(key, 2)
+        )
+
     def __call__(self, data, vec, pe):
         img, txt = data["img"], data["txt"]
-        # img = self.weight(img)
-        # txt = self.weight(txt)
+        txt_len = txt.shape[-2]
+
+        img_mod1, img_mod2 = self.img_mod(vec)
+        txt_mod1, txt_mod2 = self.txt_mod(vec)
+
+        img_normed = self.img_norm1(img)
+        img_modulated = img_mod1(img_normed)
+        img_q, img_k, img_v = self.img_attn.qkv(img_modulated)
+        
+        txt_normed = self.txt_norm1(txt)
+        txt_modulated = txt_mod1(txt_normed)
+        txt_q, txt_k, txt_v = self.txt_attn.qkv(txt_modulated)
+        
+        q = jnp.concatenate((txt_q, img_q), axis=-3)
+        k = jnp.concatenate((txt_k, img_k), axis=-3)
+        v = jnp.concatenate((txt_v, img_v), axis=-3)
+        
+        attn = attention(q, k, v, pe)
+        txt_attn, img_attn = attn[..., :txt_len, :], attn[..., txt_len:, :]
+
+        img = img + img_mod1.gate * self.img_attn.o_proj(img_attn)
+        img = img + img_mod2.gate * self.img_mlp(img_mod2(self.img_norm2(img)))
+    
+        txt = txt + txt_mod1.gate * self.txt_attn.o_proj(txt_attn)
+        txt = txt + txt_mod2.gate * self.txt_mlp(txt_mod2(self.txt_norm2(txt)))
+        
         return dict(img=img, txt=txt)
 
 
@@ -307,12 +400,9 @@ class SingleStreamBlock(eqx.Module):
     config: DiFormerConfig
     head_dim: int
 
-    attn_qkv_proj: VLinear
-    mlp_in_proj: VLinear
-    attn_o_proj: VLinear
-    mlp_out_proj: VLinear
+    attn: SelfAttention
+    mlp: MLP
 
-    qk_norm: QKNorm
     pre_norm: VLayerNorm
     modulation: Modulation
     
@@ -321,33 +411,9 @@ class SingleStreamBlock(eqx.Module):
 
         self.head_dim = config.hidden_size // config.num_heads
         
-        mlp_hidden_dim = int(config.hidden_size * config.mlp_ratio)
-        self.attn_qkv_proj = VLinear(
-            config.hidden_size,
-            config.hidden_size * 3,
-            key=key,
-            use_bias=True,
-        )
-        self.mlp_in_proj = VLinear(
-            config.hidden_size,
-            mlp_hidden_dim,
-            key=key,
-            use_bias=True,
-        )
-        self.attn_o_proj = VLinear(
-            config.hidden_size,
-            config.hidden_size,
-            key=key,
-            use_bias=True,
-        )
-        self.mlp_out_proj = VLinear(
-            mlp_hidden_dim,
-            config.hidden_size,
-            key=key,
-            use_bias=True
-        )
+        self.attn = SelfAttention(config.hidden_size, config.num_heads, qkv_bias=config.qkv_bias, key=key)
+        self.mlp = MLP(config, key=key)
 
-        self.qk_norm = QKNorm(self.head_dim)
         self.pre_norm = VLayerNorm(config.hidden_size, use_weight=False, use_bias=False, eps=1e-6)
         
         self.modulation = Modulation(config.hidden_size, double=False, key=key)
@@ -357,22 +423,10 @@ class SingleStreamBlock(eqx.Module):
         x = self.pre_norm(data)
         x = mod(x)
         
-        qkv = self.attn_qkv_proj(x)
-        mlp = self.mlp_in_proj(x)
-        mlp = jax.nn.gelu(mlp, approximate=True)
+        attn_out = self.attn(x, pe)
+        mlp_out = self.mlp(x)
 
-        q, k, v = rearrange(
-            qkv,
-            "... n (qkv heads dim) -> qkv ... n heads dim",
-            qkv=3,
-            dim=self.head_dim,
-        )
-        q, k = self.qk_norm(q, k, v)
-        
-        attn = attention(q, k, v, pe=pe)
-        output = self.mlp_out_proj(mlp) + self.attn_o_proj(attn)
-
-        return x + mod.gate * output
+        return x + mod.gate * (attn_out + mlp_out)
 
 
 class DiFormer(eqx.Module):
@@ -456,23 +510,16 @@ class DiFormer(eqx.Module):
         pe = self.pe_embedder(ids)
 
         data = dict(img=img, txt=txt)
-        for block in self.double_blocks:
+        for block in tqdm(self.double_blocks):
             data = block(data, vec=vec, pe=pe)
 
         txt, img = data["txt"], data["img"]
         data = jnp.concatenate((txt, img), -2)
-        for block in self.single_blocks:
+        for block in tqdm(self.single_blocks):
             data = block(data, vec=vec, pe=pe)
         img = img[..., txt.shape[-2]:, :]
 
         img = self.final_layer(img, vec)
-
-        # weights, logic = eqx.partition(self.block, filter_spec=eqx.is_array)
-        # logic = partial(logic, vec=vec, pe=pe)
-        # img = jax.lax.scan(lambda data, weights: eqx.combine((weights, logic))(data),
-        #                    dict(img=img, txt=txt),
-        #                    weights
-        #                    )["img"]
         
         return img
 
