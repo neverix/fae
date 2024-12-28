Try Fry plots, fix frequency calculation, run on different layers and sites

data_analyzer.py
@@ -1,71 +1,114 @@
 #%%
 from glob import glob
 import numpy as np
-from ml_dtypes import bfloat16
 import matplotlib.pyplot as plt
 
-datas = []
-for i in glob("somewhere/td/*.npy"):
-    datas.append(np.load(i))
-data = np.concatenate(datas)
-data.shape
+text_data = np.concatenate(list(map(np.load, glob("somewhere/td/*.npy"))))
+image_double_data = np.concatenate(list(map(np.load, glob("somewhere/isd/*.npy"))))
+image_single_data = np.concatenate(list(map(np.load, glob("somewhere/idd/*.npy"))))
 #%%
 plt.xscale("log")
 # norm histogram
-plt.hist(np.linalg.norm(data, axis=1), bins=1000, label="norm")
+plt.hist(np.linalg.norm(text_data, axis=1), bins=1000, label="norm")
 # norm away from mean histogram
-plt.hist(np.linalg.norm(data - np.mean(data, axis=0), axis=1), bins=1000, label="norm away from mean")
+plt.hist(np.linalg.norm(text_data - np.mean(text_data, axis=0), axis=1), bins=1000, label="norm away from mean")
 plt.legend()
 plt.show()
 #%%
 # PCA
 from sklearn.decomposition import PCA
-pca = PCA()
-pca.fit(data)
-data_pca = pca.transform(data)
-plt.scatter(data_pca[:, 0], data_pca[:, 1])
+text_pca = PCA()
+text_pca.fit(text_data)
+text_data_pca = text_pca.transform(text_data)
+#%%
+plt.scatter(text_data_pca[:, 0], text_data_pca[:, 1], c=(np.arange(len(text_data_pca)) % 512) / 512., s=0.01)
 plt.xlabel("PC1")
 plt.ylabel("PC2")
+plt.colorbar()
 #%%
-plt.xscale("log")
-plt.hist(data_pca[:, 0], bins=1000)
+# plt.xscale("log")
+plt.hist(text_data_pca[:, 0], bins=1000)
 plt.xlabel("PC1")
 
-flagged = data_pca[:, 0] > 500
+flagged = text_data_pca[:, 0] > 500
 flagged.sum()
 #%%
 plt.plot(
-    np.corrcoef(x=data_pca[flagged],
+    np.corrcoef(x=text_data_pca[flagged],
                 rowvar=False)[0, 1:] ** 2)
 plt.xlabel("Principal component")
 plt.ylabel("R^2 with PC1 on samples affected by the outlier direction")
 #%%
-plt.plot(np.sort(data.std(axis=0))[::-1])
+plt.plot(np.sort(text_data.std(axis=0))[::-1])
 plt.xlabel("Rank of residual neuron by standard deviation")
 plt.ylabel("Standard deviation")
 plt.xscale("log")
 plt.yscale("log")
 #%%
-plt.plot(data.max(axis=0) - data.min(axis=0))
+plt.plot(text_data.max(axis=0) - text_data.min(axis=0))
+plt.plot(text_data.std(axis=0))
 plt.xlabel("Residual neuron #")
 plt.ylabel("Amplitude of activation")
-plt.ylim(0, 14000)
+# plt.ylim(0, 14000)
 #%%
 # Variance explained by PCA
-pca = PCA()
-pca.fit(data)
+double_pca = PCA()
+double_pca.fit(image_double_data)
+single_pca = PCA()
+single_pca.fit(image_single_data)
 #%%
-plt.plot(pca.explained_variance_ratio_)
+plt.plot(double_pca.explained_variance_ratio_, label="double")
+plt.plot(single_pca.explained_variance_ratio_, label="single")
+plt.xlabel("Principal component")
+plt.ylabel("Variance explained")
+plt.legend()
 plt.xscale("log")
 plt.yscale("log")
 # data is very low-rank!
 # %%
-# for comparison, let's look at data from an LM
-llama_data = np.load("somewhere/llama.npz")["arr_0"]
-pca = PCA()
-pca.fit(llama_data)
+# visualize double/single neuron amplitudes
+plt.plot(image_single_data.max(axis=0) - image_single_data.min(axis=0), label="single")
+plt.plot(image_double_data.max(axis=0) - image_double_data.min(axis=0), label="double")
+plt.xlabel("Residual neuron #")
+plt.ylabel("Amplitude of activation")
+plt.legend()
+plt.show()
 #%%
-plt.plot(pca.explained_variance_ratio_[1:])
+# Same data, but sorted by double neuron std
+double_sort_order = np.argsort(image_double_data.std(axis=0))[::-1]
+single_sort_order = np.argsort(image_single_data.std(axis=0))[::-1]
+plt.plot(image_double_data.std(axis=0)[double_sort_order], label="double")
+# plt.plot(image_single_data.std(axis=0)[double_sort_order], label="single")
+plt.plot(image_single_data.std(axis=0)[single_sort_order], label="single")
+plt.xlabel("Index sorted by neuron std")
+plt.ylabel("Standard deviation")
+plt.yscale("log")
 plt.xscale("log")
-# plt.yscale("log")
+plt.legend()
+plt.show()
 # %%
+plt.scatter(image_single_data.std(axis=0), text_data.std(axis=0), s=10)
+plt.ylim(0, 1000)
+#%%
+double_pca_data = double_pca.transform(image_double_data)
+plt.scatter(double_pca_data[:, 0], double_pca_data[:, 1], s=10)
+plt.xlabel("PC1")
+plt.ylabel("PC2")
+#%%
+double_norms = np.linalg.norm(image_double_data, axis=1)
+plt.hist(double_norms, bins=1000)
+plt.show()
+#%%
+plt.plot(np.sort(double_pca.explained_variance_ratio_)[::-1])
+plt.plot(np.sort(single_pca.explained_variance_ratio_)[::-1])
+plt.xscale("log")
+plt.yscale("log")
+plt.show()
+#%%
+plt.plot(np.sort(image_double_data.var(axis=0))[::-1])
+plt.plot(np.sort(image_single_data.var(axis=0))[::-1])
+plt.plot(np.sort(text_data.var(axis=0))[::-1])
+
+plt.xscale("log")
+plt.yscale("log")
+plt.show()

main.py
@@ -16,6 +16,7 @@ CACHE_DIRECTORY = "somewhere/maxacts"
 HEIGHT, WIDTH = 16, 16
 vae = FluxVAE("somewhere/taef1/taef1_encoder.onnx", "somewhere/taef1/taef1_decoder.onnx")
 cache_dir = Path(CACHE_DIRECTORY)
+image_activations_dir = cache_dir / "image_activations" 
 image_cache_dir = Path("somewhere/img_cache")
 if image_cache_dir.exists():
     shutil.rmtree(image_cache_dir)
@@ -59,8 +60,7 @@ def top_features():
     # metric[maxima < 5] = np.inf
     # correct_order = np.argsort(metric)
     # matches = np.arange(len(scored_storage))[maxima > 3.5]
-    matches = np.arange(len(scored_storage))[maxima > 3]
-    # matches = np.arange(len(scored_storage))[frequencies > 0.0002]
+    matches = np.arange(len(scored_storage))[(maxima > 3) & (frequencies < 0.0031)]
     correct_order = np.random.permutation(matches)
     top_few = correct_order[:256].tolist()
     return Div(
@@ -78,6 +78,22 @@ def feature_counts():
     counts = {key: int(val) for key, val in enumerate(counts)}
     return JSONResponse(counts)
 
+@rt("/fry_plot")
+def fry_plot():
+    counts = scored_storage.key_counts()
+    maxima = scored_storage.key_maxima()
+    img_list = list(image_activations_dir.glob("*.npz"))
+    print(img_list)
+    batch_numbers = [int(img.stem.partition("_")[0]) for img in img_list]
+    seq_numbers = [int(img.stem.split("_")[1]) for img in img_list]
+    frequencies = counts.astype(np.float64) / (max(batch_numbers) * (max(seq_numbers) + 1))
+    return plotly2fasthtml(px.scatter(
+        x=frequencies,
+        y=maxima,
+        labels={"x": "Frequency", "y": "Max Activation"},
+        title="Fry Plot"
+    ))
+
 @rt("/maxacts/{feature_id}")
 def maxacts(feature_id: int):
     rows = scored_storage.get_rows(feature_id)
@@ -94,8 +110,8 @@ def maxacts(feature_id: int):
 
     # Prepare images and cards
     imgs = []
-    for (step, idx), grid in sorted(grouped_rows.items(), key=lambda x: x[1].max(), reverse=True):
-        full_activations = np.load(cache_dir / "image_activations" / f"{step}_{idx}.npz")
+    for (step, idx), grid in sorted(grouped_rows.items(), key=lambda x: x[1].max(), reverse=True)[:20]:
+        full_activations = np.load(image_activations_dir / f"{step}_{idx}.npz")
         gravel = grid.ravel()
         k = full_activations["arr_0"].shape[1]
         for i, (f, w) in enumerate(zip(full_activations["arr_0"].ravel(), full_activations["arr_1"].ravel())):

src/fae/sae_trainer.py
@@ -13,7 +13,7 @@ from loguru import logger
 from tqdm.auto import tqdm
 from optax._src.linear_algebra import global_norm
 import optax
-import heapq
+import os
 import queue
 import jax.numpy as jnp
 import equinox as eqx
@@ -28,7 +28,7 @@ from typing import Sequence
 from concurrent.futures import ThreadPoolExecutor
 import asyncio
 from .sae_common import SAEConfig, SAEOutputSaver
-from .interp_globals import post_double_stream
+from .interp_globals import post_double_stream, post_single_stream
 import threading
 
 
@@ -528,7 +528,7 @@ class SAEOverseer:
         return self.sae_trainer.mesh
 
 
-def main(train_mode: bool = True, restore: bool = False):
+def main(train_mode: bool = True, restore: bool = False, seq_mode = "img", block_type = "double", layer = 18):
     logger.info("Loading dataset")
     prompts_dataset = load_dataset("opendiffusionai/cc12m-cleaned")
     prompts_iterator = prompts_dataset["train"]["caption_llava_short"]
@@ -537,7 +537,7 @@ def main(train_mode: bool = True, restore: bool = False):
     logger.info("Creating SAE trainer")
     config = SAEConfig(
         do_update=train_mode,
-        seq_mode="img",
+        seq_mode=seq_mode,
         **(dict(sae_train_every=1,
         sae_batch_size_multiplier=1)
         if not train_mode else {}),
@@ -568,7 +568,9 @@ def main(train_mode: bool = True, restore: bool = False):
             appeared_prompts |= new_prompts
         key = jax.random.key(step)
         logger.remove()
-        with post_double_stream.capture(18) as reaped:
+        with (post_double_stream if block_type == "double" else
+              post_single_stream if block_type == "single" else NotImplemented
+              ).capture(layer) as reaped:
             images = ensemble.sample(
                 prompts,
                 decode_latents=False,
@@ -579,10 +581,15 @@ def main(train_mode: bool = True, restore: bool = False):
             )
         assert isinstance(images, jnp.ndarray)  # to silence mypy
         logger.add(sys.stderr, level="INFO")
-        training_data = jnp.concatenate((reaped[18]["txt"], reaped[18]["img"]), axis=-2)[0]
+        if block_type =="double":
+            training_data = jnp.concatenate((reaped[layer]["txt"], reaped[layer]["img"]), axis=-2)[0]
+        else:
+            training_data = reaped[layer]
         training_data = config.cut_up(training_data)
-        # if step < 100:
-            # np.save(f"somewhere/td/{step}.npy", training_data.astype(np.float32))
+        if step < 25:
+            pardir = f"somewhere/{'t' if seq_mode == 'txt' else ('is' if block_type == 'single' else 'id')}d"
+            os.makedirs(pardir, exist_ok=True)
+            np.save(f"{pardir}/{step}.npy", training_data.astype(np.float32))
         activation_cache.append(np.asarray(training_data))
         if len(activation_cache) >= config.sae_train_every:
             assert config.sae_train_every % config.sae_batch_size_multiplier == 0